93013361796a456d30a42e0373ba15421a55f467
Instituto Precisa Ser <joyceassis96@gmail.com>
2024-03-20 22:55:02 -0300

    funcao lambda

---
+++ lambda-nodejs18.x/hello-world/app.mjs
@@ -0,0 +1,28 @@
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+
+export async function lambdaHandler(event) {
+    if (event.name) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: `Olá, ${event.name}!` })
+        };
+    } else {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: 'Nome não fornecido!' })
+        };
+    }
+}
+
+
